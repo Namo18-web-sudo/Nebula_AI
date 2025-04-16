d9e256f64b6df9b497976cfb8872e9bd6e21f8b6
Namo18-web-sudo <nebula.foundation.official@gmail.com>
2025-04-16 14:39:07 +0530

    Update server.py

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ CORS(app)
 # 🔐 Google Gemini API Key
 API_KEY = "AIzaSyAi-T726fwJy0YdaTlFxwqwIge1Ic36_eM"
 genai.configure(api_key=API_KEY)
-model = genai.GenerativeModel("gemini-pro-vision")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 # ✅ Home Route
 @app.route("/")
